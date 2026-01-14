2ccb18da33526c799a2786a7b1466c744953aa5a
widataisn <widataisn@users.noreply.github.com>
2026-01-14 11:35:57 +0700

    Menghapus chatbot pribadi + hapus 2 branch master dan env

--- Chatbot_Pribadi/shell/run_vllm.sh
+++
@@ -1,6 +0,0 @@
-python3 -m vllm.entrypoints.openai.api_server \
-    --model Qwen/Qwen2-VL-7B-Instruct \
-    --host 0.0.0.0 \
-    --port 8002 \
-    --trust-remote-code \
-    --gpu-memory-utilization 0.9 &
